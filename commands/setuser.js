5075f4e47444394c4e84a47b6bca49230ec69410
Alex Howe <howie242000@gmail.com>
2021-01-07 17:02:21 -0500

    Added an option to not use your chess.com name in discord

--- commands/setuser.js
+++ commands/setuser.js
@@ -2,26 +2,32 @@ const fs = require('fs');
 const eloHandler = require('./../modules/updateELO.js');
 const ChessWebAPI = require('chess-web-api');
 const chess = new ChessWebAPI();
+const delay = require('delay');
+
 
 module.exports = {
     name: 'Set User',
     command: 'setuser',
     description: "Saves your Chess.com username to access your player data.",
-    usage: '`setuser`',
+    usage: '`setuser [username] {nickname}`',
     async execute(message, args) {
-        if(!args.length || args.length > 1) {
+        if(!args.length || args.length > 2) {
             message.channel.send(`Usage: ${this.usage}`);
             return;
         }
+        let nickname = args.length === 2 ? args[1] : null;
 
         let userJSON = JSON.parse(fs.readFileSync('./data/users.json'));
 
         try {
             let player = await chess.getPlayer(args[0]);
             if(player.body) {
-                console.log(player);
+                console.log(player.body.username);
                 Object.defineProperty(userJSON, message.author.id, {
-                    value: args[0],
+                    value: {
+                        username: args[0],
+                        nickname: nickname
+                    },
                     writable: true,
                     configurable: true,
                     enumerable: true
@@ -29,12 +35,13 @@ module.exports = {
                 fs.writeFile('./data/users.json', JSON.stringify(userJSON, null, 4), err => { if(err) { throw err; } });
                 message.channel.send(`Chess.com user for <@!${message.author.id}> set to \`${args[0]}\``);
                 message.delete();
-                eloHandler.updateELO();
             }
         }
         catch(e) {
             message.channel.send('User does not exist.');
             return;
         }
+        await delay(500);
+        eloHandler.updateELO();
     }
 }
--- modules/updateELO.js
+++ modules/updateELO.js
@@ -1,24 +1,25 @@
 const fs = require('fs');
 const ChessWebAPI = require('chess-web-api');
 const chess = new ChessWebAPI();
+const delay = require('delay');
 
 module.exports = {
     async updateELO(client) {
         try {
             let userJSON = JSON.parse(fs.readFileSync('./data/users.json'));
             for(const key of Object.keys(userJSON)) {
+                console.log(userJSON[key]);
                 let guild = await client.guilds.resolve('796476454384566343');
                 if(key === guild.ownerID) {
                     console.log(`Cannot update owner's nickname`);
                     continue;
                 }
                 let member = await guild.members.fetch(key);
-                //console.log(member);
-                let chessStats = await chess.getPlayerStats(userJSON[key]);
+                let chessStats = await chess.getPlayerStats(userJSON[key].username);
                 let rating = chessStats.body.chess_rapid.last.rating;
-                //console.log(rating);
-                member.setNickname(`${userJSON[key]} [${rating}]`);
-                console.log(`Updated rating for ${userJSON[key]}`);
+                
+                member.setNickname(`${userJSON[key].nickname || userJSON[key].username} [${rating}]`).catch(e => { console.log('Cannot update nickname'); });
+                console.log(`Updated rating for ${userJSON[key].nickname || userJSON[key].username}`);
             }
         }
         catch(e) {
